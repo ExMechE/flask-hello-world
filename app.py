153cdf62f2225cee57d8bc574ef4da2e865770cc
ExMechE <89749600+ExMechE@users.noreply.github.com>
2024-04-02 12:09:44 -0400

    Update app.py /db_select

--- app.py
+++ app.py
@@ -44,3 +44,22 @@ def inserting():
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Populated"
+
+@app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect("postgres://lab_10_sql_user:HzOhwqZJynf1mEUTvbcPbKLHvnxGm5Gj@dpg-co62cg63e1ms73behhlg-a/lab_10_sql")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+        ''')
+    records = cur.fetchall()
+    conn.close()
+    response_string =""
+    response_string+="<table>"
+    for player in records:
+        response string+="<tr>"
+        for info in player:
+            response_string+="<td>{}</td>".format(info)
+        response_string+="</tr>"
+    response_string+="</table>"
+    return response_string
